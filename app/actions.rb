18bb79bdba23687354178feef0e6b739a9f3d0db
James McKeen <jamescmckeen@gmail.com>
2016-04-26 21:47:43 -0700

    debugging ajax, testing redirects and status 200 on the controller

--- app/actions.rb
+++ app/actions.rb
@@ -5,22 +5,34 @@ get '/' do
 end
 
 post '/generate' do
+  status 200
   @sequence = Sequence.new
   @sequence.sequence_array = @sequence.generate(params[:time].to_i, params[:start], params[:focus])
   @sequence.save
+  redirect '/'
 end
 
 get '/generate' do
+  status 200
   @sequence = Sequence.last.sequence_array
+  redirect '/'
 
 end
 
 post '/rating' do
+  status 200
+
   @sequence = Sequence.last
   @sequence.rating = params[:rating]
   @sequence.save
+  redirect '/'
+
 end
 
 get '/rating' do
+  status 200
+  
   @sequence = Sequence.where(rating: 5).order("RANDOM()").last.sequence_array
+  redirect '/'
+
 end
