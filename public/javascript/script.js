9ae3b45a2f3793b634ddf8ec4f47d636798855d6
James McKeen <jamescmckeen@gmail.com>
2016-04-26 20:45:35 -0700

    fixing json parse error

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   $(document.body).fadeIn(3000);
 
-  var imageArray = JSON.parse(images);
+  // var imageArray = JSON.parse(images);
 
   $('#post_sequence ').submit(function(e){
     e.preventDefault();
@@ -106,7 +106,6 @@ $('.arrowDown').click(function(){
       $.ajax({
         url: '/rating',
         method: 'get',
-        dataType: 'jsonp',
         success: function(data){
           var poseArray = JSON.parse(data);
           for (var i = 0; i < poseArray.length; i++){
@@ -155,7 +154,7 @@ $('.arrowDown').click(function(){
   };
 
   function startSlideShow(loadslides){  
-    var intervalInc = 0;
+    var intervalInc = 0
     var rated = false;
     $('#slideshow img').remove();
     $('#slideshow .pose-name').remove();
@@ -213,5 +212,8 @@ $('.arrowDown').click(function(){
       }, 250);
     }
   });
+
+var imageArray = [{"Mountain": "/images/yoga_for_app/Mountain.jpg"}, {"Warrior 1": "/images/yoga_for_app/Warrior_1.jpg"}, {"Warrior 2": "/images/yoga_for_app/Warrior_2.jpg"}, {"Warrior 3": "/images/yoga_for_app/Warrior_3.jpg"}, {"Extended Side Angle": "/images/yoga_for_app/Extended_Side_Angle.jpg"}, {"Reverse Warrior": "/images/yoga_for_app/Reverse_Warrior.jpg"}, {"High Lunge": "/images/yoga_for_app/High_Lunge.jpg"}, {"Pyramid": "/images/yoga_for_app/Pyramid.jpg"}, {"Twisted Lunge": "/images/yoga_for_app/Twisted_Lunge.jpg"}, {"Five-Pointed Star": "/images/yoga_for_app/Five-Pointed_Star.jpg"}, {"Wide-Legged Fold": "/images/yoga_for_app/Wide-Legged_Fold.jpg"}, {"Goddess Squat": "/images/yoga_for_app/Goddess_Squat.jpg"}, {"Standing Splits": "/images/yoga_for_app/Standing_Splits.jpg"}, {"Tree Pose": "/images/yoga_for_app/Tree_Pose.jpg"}, {"Dancers Pose": "/images/yoga_for_app/Dancers_Pose.jpg"}, {"Extended Hand-To-Big-Toe": "/images/yoga_for_app/Extended_Hand-To-Big-Toe.jpg"}, {"Knee-To-Chest": "/images/yoga_for_app/Knee-To-Chest.jpg"}, {"Eagle": "/images/yoga_for_app/Eagle.jpg"}, {"Triangle": "/images/yoga_for_app/Triangle.jpg"}, {"Half Moon": "/images/yoga_for_app/Half_Moon.jpg"}, {"Reverse Triangle": "/images/yoga_for_app/Reverse_Triangle.jpg"}, {"Runners Lunge": "/images/yoga_for_app/Runners_Lunge.jpg"}, {"Sleeping Pigeon": "/images/yoga_for_app/Sleeping_Pigeon.jpg"}, {"Low Lunge": "/images/yoga_for_app/Low_Lunge.jpg"}, {"Head-To-Knee-Pose": "/images/yoga_for_app/Head-To-Knee-Pose.jpg"}, {"Downward Facing Dog": "/images/yoga_for_app/Downward_Facing_Dog.jpg"}, {"Bharadvajas Twist": "/images/yoga_for_app/Bharadvajas_Twist.jpg"}, {"Bound Angle": "/images/yoga_for_app/Bound_Angle.jpg"}, {"Cow Face": "/images/yoga_for_app/Cow_Face.jpg"}, {"Easy Pose": "/images/yoga_for_app/Easy_Pose.jpg"}, {"Fire Log": "/images/yoga_for_app/Fire_Log.jpg"}, {"Fish": "/images/yoga_for_app/Fish.jpg"}, {"Gate": "/images/yoga_for_app/Gate.jpg"}, {"Happy Baby": "/images/yoga_for_app/Happy_Baby.jpg"}, {"Head-to-knee Forward Bend": "/images/yoga_for_app/Head-to-knee_Forward_Bend.jpg"}, {"Hero": "/images/yoga_for_app/Hero.jpg"}, {"Toes Pose": "/images/yoga_for_app/Toes_Pose.jpg"}, {"Heron": "/images/yoga_for_app/Heron.jpg"}, {"Legs-Up-The-Wall": "/images/yoga_for_app/Legs-Up-The-Wall.jpg"}, {"Marichis Pose": "/images/yoga_for_app/Marichis_Pose.jpg"}, {"Plow": "/images/yoga_for_app/Plow.jpg"}, {"Reclining Hand-to-Big-Toe Pose": "/images/yoga_for_app/Reclining_Hand-to-Big-Toe_Pose.jpg"}, {"Lotus": "/images/yoga_for_app/Lotus.jpg"}, {"Seated Forward Bend": "/images/yoga_for_app/Seated_Forward_Bend.jpg"}, {"Side-Reclining Leg Lift": "/images/yoga_for_app/Side-Reclining_Leg_Lift.jpg"}, {"Shoulder Stand": "/images/yoga_for_app/Shoulder_Stand.jpg"}, {"Wide-Angle Seated Fold": "/images/yoga_for_app/Wide-Angle_Seated_Fold.jpg"}, {"Supine Twist": "/images/yoga_for_app/Supine_Twist.jpg"}, {"Corpse Pose": "/images/yoga_for_app/Corpse_Pose.jpg"}, {"Seated Meditation": "/images/yoga_for_app/Seated_Meditation.jpg"}, {"Boat": "/images/yoga_for_app/Boat.jpg"}, {"Dolphin": "/images/yoga_for_app/Dolphin.jpg"}, {"Plank": "/images/yoga_for_app/Plank.jpg"}, {"Four-Limb_Staff": "/images/yoga_for_app/Four-Limb_Staff.jpg"}, {"Headstand": "/images/yoga_for_app/Headstand.jpg"}, {"Upward Plank": "/images/yoga_for_app/Upward_Plank.jpg"}, {"Reclined knee-to-chest": "/images/yoga_for_app/Reclined_knee-to-chest.jpg"}, {"Table Top": "/images/yoga_for_app/Table_Top.jpg"}, {"Chair": "/images/yoga_for_app/Chair.jpg"}, {"Crow": "/images/yoga_for_app/Crow.jpg"}, {"Knee-to-Arm Plank": "/images/yoga_for_app/Knee-to-Arm_Plank.jpg"}, {"Arm-Balance Split": "/images/yoga_for_app/Arm-Balance_Split.jpg"}, {"Corpse": "/images/yoga_for_app/Corpse.jpg"}, {"Childs Pose": "/images/yoga_for_app/Childs_Pose.jpg"}, {"Forward Fold": "/images/yoga_for_app/Forward_Fold.jpg"}, {"Wheel": "/images/yoga_for_app/Wheel.jpg"}, {"Bridge": "/images/yoga_for_app/Bridge.jpg"}, {"Camel": "/images/yoga_for_app/Camel.jpg"}, {"Reclining Hero Pose": "/images/yoga_for_app/Reclining_Hero_Pose.jpg"}, {"Cobra": "/images/yoga_for_app/Cobra.jpg"}, {"Extended Puppy Dog": "/images/yoga_for_app/Extended_Puppy_Dog.jpg"}, {"Cat": "/images/yoga_for_app/Cat.jpg"}, {"Cow": "/images/yoga_for_app/Cow.jpg"}, {"Lie on Stomach": "/images/yoga_for_app/Lie_on_Stomach.jpg"}, {"Locust": "/images/yoga_for_app/Locust.jpg"}, {"Bow": "/images/yoga_for_app/Bow.jpg"}, {"Sphinx": "/images/yoga_for_app/Sphinx.jpg"}, {"Squat": "/images/yoga_for_app/Squat.jpg"}, {"Standing Backbend": "/images/yoga_for_app/Standing_Backbend.jpg"}, {"Wild Thing": "/images/yoga_for_app/Wild_Thing.jpg"}, {"Three-Legged Downward Facing Dog": "/images/yoga_for_app/Three-Legged_Downward_Facing_Dog.jpg"}, {"Fallen Triangle": "/images/yoga_for_app/Fallen_Triangle.jpg"}, {"Reclining Bound Angle Pose": "/images/yoga_for_app/Reclining_Bound_Angle_Pose.jpg"}, {"Final Downward Facing Dog": "/images/yoga_for_app/Final_Downward_Facing_Dog.jpg"}, {"Ragdoll": "/images/yoga_for_app/Ragdoll.jpg"}, {"Stand at Top of Mat": "/images/yoga_for_app/Stand_at_Top_of_Mat.jpg"}, {"Step to Lunge": "/images/yoga_for_app/Step_to_Lunge.jpg"}, {"Samasthitihi": "/images/yoga_for_app/Samasthitihi.jpg"}, {"Upward Salute": "/images/yoga_for_app/Upward_Salute.jpg"}, {"Handstand": "/images/yoga_for_app/Handstand.jpg"}, {"Float to Top of Mat": "/images/yoga_for_app/Float_to_Top_of_Mat.jpg"}, {"Upward Facing Dog": "/images/yoga_for_app/Upward_Facing_Dog.jpg"}, {"Big Toe Pose": "/images/yoga_for_app/Big_Toe_Pose.jpg"}, {"Halfway Lift": "/images/yoga_for_app/Halfway_Lift.jpg"}, {"Hands On Hips": "/images/yoga_for_app/Hands_On_Hips.jpg"}, {"Savasana": "/images/yoga_for_app/Corpse.jpg"}, {"Transition to Top of Mat": "/images/yoga_for_app/Transition_to_Top_of_Mat.jpg"}, {"Staff": "/images/yoga_for_app/Staff.jpg"}]
+
 
 });
