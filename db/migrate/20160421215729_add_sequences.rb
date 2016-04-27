3b79ca7e5877d390d4730d90d7f906eab88a08c5
James McKeen <jamescmckeen@gmail.com>
2016-04-27 13:46:35 -0700

    reverting changes back to localhost state

--- db/migrate/20160421215729_add_sequences.rb
+++ db/migrate/20160421215729_add_sequences.rb
@@ -1,7 +1,7 @@
 class AddSequences < ActiveRecord::Migration
   def change
     create_table :sequences do |t|
-      t.string :sequence_array, array:true, default: [2]
+      t.string :sequence_array, array:true, default: []
       t.integer :rating
     end
   end
