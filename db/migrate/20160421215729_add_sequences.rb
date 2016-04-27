c1da74ef80328d400787852aa600baf467af7293
James McKeen <jamescmckeen@gmail.com>
2016-04-27 13:00:05 -0700

    500 error resolved, attempting fix of responsetext from string to array of strings. Changing table in add_sequences for postgresql

--- db/migrate/20160421215729_add_sequences.rb
+++ db/migrate/20160421215729_add_sequences.rb
@@ -1,8 +1,10 @@
 class AddSequences < ActiveRecord::Migration
   def change
     create_table :sequences do |t|
-      t.string :sequence_array, array: true, default: []
+      t.string[][] :sequence_array,
       t.integer :rating
     end
   end
 end
+ # Works locally, but not on heroku
+ #array: true, default: []
